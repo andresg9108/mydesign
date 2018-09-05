70eaca454f62b7311a418c7c12120f108ec1725c
Andres Gonzalez <andresg9108@gmail.com>
2018-09-05 15:59:55 -0500

    Funcionalidad de ventana modal.

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,15 @@ var g_sRouteTemplate = 'src/template/';
 
 $(function(){
 });
+
+/*
+*/
+function openModalWindow(sSelector){
+	$('#'+sSelector).addClass('is-active');
+}
+
+/*
+*/
+function closeModalWindow(sSelector){
+	$('#'+sSelector).removeClass('is-active');
+}
