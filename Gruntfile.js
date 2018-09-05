e01fc27df532d51a833a2a325c84a287dac558f9
Andres Gonzalez <andresg9108@gmail.com>
2018-09-05 09:59:48 -0500

    Cargando estructura del proyecto.

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+    // Route
+    var aRoute = [
+        './web/sidebar/*'
+    ];
+    
+    // Sass
+    var aRouteSass = ['./src/sass/*'];
+    
+    // Js
+    var aRouteJs = ['./src/js/*'];
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed', noCache: true
+                },
+                files: [{
+                    expand: true,
+                    cwd:    "src/sass/",
+                    src:    ["*.sass"],
+                    dest: "src/css/",
+                    ext:    ".css"
+                }]
+            }
+        },
+
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: true
+            },
+            load_route: {
+                files: aRoute
+            },
+            load_sass: {
+                files: aRouteSass,
+                tasks: ['sass']
+            },
+            load_js: {
+                files: aRouteJs
+            }
+        }
+        
+    });
+
+    grunt.registerTask('tPy', function(){
+        grunt.util.spawn({
+            cmd: 'python',
+            args: ['load.py', '-l'],
+            opts: {stdio: 'inherit'},
+        });
+    });
+    
+    grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-contrib');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+};
