a70137ce18e7439e8d59f5a44fe4e66b66525fdf
Andres Gonzalez <andresg9108@gmail.com>
2018-09-08 09:38:44 -0500

    sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,28 @@ function openModalWindow(sSelector){
 function closeModalWindow(sSelector){
 	$('#'+sSelector).removeClass('is-active');
 }
+
+/*
+*/
+function sideMenuController(){
+	let oBackground = $(".my-sidebar #background");
+	let oMenu = $(".my-sidebar #menu");
+    let iTime = 400;
+
+    if(oBackground.css('display') != 'block'){
+    	oBackground.css('display','block');
+
+    	oMenu.animate({
+    		width: '100%'
+    	}, iTime, function() {
+    		oMenu.addClass('is-active');
+    	});
+    }else{
+    	oMenu.removeClass('is-active');
+    	oMenu.animate({
+    		width: 0
+    	}, iTime, function() {
+    		oBackground.css('display','none');
+    	});
+    }
+}
